13efd075328f157a7a1e29a8b0b5bd4628caef25
qinhaojie <qinhaojie1992@163.com>
2015-08-13 10:29:56 +0800

    修改文件

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,13 @@
 var gulp = require('gulp');
 var less = require('gulp-less');
-
+var sourcemap = require('gulp-sourcemaps');
 gulp.task('less', function () {
-    gulp.src('less/bootstrap.less')
+    gulp.src('less/main.less')
+        .pipe(sourcemap.init())
         .pipe(less())
+        .pipe(sourcemap.write('.map'))
         .pipe(gulp.dest('css/'));
 });
-gulp.task('css', function () {
+gulp.task('css',['less'], function () {
     gulp.watch('less/**/*.less', ['less']); //当所有less文件发生改变时，调用testLess任务
 });
